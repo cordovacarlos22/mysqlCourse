50945372081e1b1a86a03e081466bffde2f43b76
carloscordova.dev <cordovamanuel22@icloud.com>
2024-09-01 09:33:47 -0700

    feat: :sparkles: introduce  sql commants for select

---
+++ Statements/select.sql
@@ -0,0 +1,13 @@
+--? SQL SELECT COMMANDS
+
+-- * select all users from table users
+SELECT * FROM users;
+
+-- * select user names from table users
+SELECT name FROM users;
+
+-- * select user ids and names from table users
+SELECT users_id, name FROM users;
+
+
+
