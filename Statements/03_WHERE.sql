a654c4aced97e59441465d9cf04ac53be0ed3159
carloscordova.dev <cordovamanuel22@icloud.com>
2024-09-01 10:16:57 -0700

    feat: :sparkles: add more commants

--- Statements/03_WHERE.sql
+++ Statements/03_WHERE.sql
@@ -1,5 +1,16 @@
--- ? sql WHERE commands
+/*
+WHERE
+Lección 9.2: https://youtu.be/OuJerKzV5T0?t=6384
+*/
 
---* returns all users where user age is equal to 15
-SELECT *  FROM users WHERE  age=15
+-- Filtra todos los datos de la tabla "users" con edad igual a 15
+SELECT * FROM users WHERE age = 15;
 
+-- Filtra todos los nombres de la tabla "users" con edad igual a 15
+SELECT name FROM users WHERE age = 15;
+
+-- Filtra todos los nombres distintos de la tabla "users" con edad igual a 15
+SELECT DISTINCT name FROM users WHERE age = 15;
+
+-- Filtra todas las edades distintas de la tabla "users" con edad igual a 15
+SELECT DISTINCT age FROM users WHERE age = 15;
